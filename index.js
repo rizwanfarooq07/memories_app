001c76e6b7f1738eb77102e8f8d17cd64740322b
Farooqui Rizwan Ahmed <rizwanfarooqui07@gmail.com>
2021-06-21 13:18:20 +0530

    update index.js

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ import postsRoutes from "./server/routes/postRoutes.js";
 const app = express();
 dotenv.config();
 
+const connectDB = async (req, res) => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
@@ -30,22 +46,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 5000;
-
-const connectDB = async (req, res) => {
-  try {
-    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(1);
-  }
-};
-
-connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
