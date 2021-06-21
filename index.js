f54fff2452621beb9232c935e61e2bfaaf570e25
Farooqui Rizwan Ahmed <rizwanfarooqui07@gmail.com>
2021-06-21 13:12:12 +0530

    updated index.js

--- index.js
+++ index.js
@@ -13,25 +13,21 @@ app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
-
-// const __dirname = path.resolve();
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/client/build")));
-
-//   app.get("/*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running!");
-//   });
-// }
-
 app.use("/posts", postsRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running!");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
