2595f76fa9f8e6f4e2602803ffd419ae2c15cb39
Farooqui Rizwan Ahmed <rizwanfarooqui07@gmail.com>
2021-06-21 12:51:19 +0530

    updated for deployement

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running!");
-  });
-}
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running!");
+//   });
+// }
 
 app.use("/posts", postsRoutes);
 
